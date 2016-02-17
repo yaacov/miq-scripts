c99c3dfa331fc9716cb449ca4a4166b5f08fa2d9
Yaacov Zamir <yzamir@gmail.com>
2016-02-17 07:46:41 +0200

    remove sudo from within script

--- virt-deploy-script-01.sh
+++ virt-deploy-script-01.sh
@@ -2,30 +2,30 @@
 
 echo "Delete VMs"
 echo "----------"
-sudo virt-deploy delete vm-test-02-centos-7.1-x86_64
-sudo virt-deploy delete vm-test-03-centos-7.1-x86_64
+virt-deploy delete vm-test-02-centos-7.1-x86_64
+virt-deploy delete vm-test-03-centos-7.1-x86_64
 
 echo "Delete VMs images from lib cache"
 echo "--------------------------------"
-sudo rm -rf /var/lib/libvirt/images/vm-test-02-centos-7.1-x86_64.qcow2
-sudo rm -rf /var/lib/libvirt/images/vm-test-03-centos-7.1-x86_64.qcow2
+rm -rf /var/lib/libvirt/images/vm-test-02-centos-7.1-x86_64.qcow2
+rm -rf /var/lib/libvirt/images/vm-test-03-centos-7.1-x86_64.qcow2
 
 echo "Delete VMs from etc/hosts"
 echo "-------------------------"
-sudo sed '/\.example\.com vm-test-/d' -i /etc/hosts
+sed '/\.example\.com vm-test-/d' -i /etc/hosts
 
 echo "Create VMs"
 echo "----------"
-sudo ip2=`virt-deploy create vm-test-02 centos-7.1 -o memory=3072 password=pass | grep 'ip address:' | cut -d: -f2`
-sudo ip3=`virt-deploy create vm-test-03 centos-7.1 -o memory=3072 password=pass | grep 'ip address:' | cut -d: -f2`
+ip2=`virt-deploy create vm-test-02 centos-7.1 -o memory=3072 password=pass | grep 'ip address:' | cut -d: -f2`
+ip3=`virt-deploy create vm-test-03 centos-7.1 -o memory=3072 password=pass | grep 'ip address:' | cut -d: -f2`
 
 echo "Edit VMs on etc/hosts"
 echo "---------------------"
-sudo echo "$ip2    vm-test-02.example.com vm-test-02" >> /etc/hosts
-sudo echo "$ip3    vm-test-03.example.com vm-test-03" >> /etc/hosts
+echo "$ip2    vm-test-02.example.com vm-test-02" >> /etc/hosts
+echo "$ip3    vm-test-03.example.com vm-test-03" >> /etc/hosts
 
 echo "Start VMs"
 echo "---------"
-sudo virt-deploy start vm-test-02-centos-7.1-x86_64
-sudo virt-deploy start vm-test-03-centos-7.1-x86_64f
+virt-deploy start vm-test-02-centos-7.1-x86_64
+virt-deploy start vm-test-03-centos-7.1-x86_64f
 
--- virt-deploy-script-03.sh
+++ virt-deploy-script-03.sh
@@ -4,15 +4,11 @@ echo "Delete VMs"
 echo "----------"
 sudo virt-deploy delete vm-test-02-centos-7.1-x86_64
 sudo virt-deploy delete vm-test-03-centos-7.1-x86_64
-sudo virt-deploy delete vm-test-04-centos-7.1-x86_64
-sudo virt-deploy delete vm-test-05-centos-7.1-x86_64
 
 echo "Delete VMs images from lib cache"
 echo "--------------------------------"
 sudo rm -rf /var/lib/libvirt/images/vm-test-02-centos-7.1-x86_64.qcow2
 sudo rm -rf /var/lib/libvirt/images/vm-test-03-centos-7.1-x86_64.qcow2
-sudo rm -rf /var/lib/libvirt/images/vm-test-04-centos-7.1-x86_64.qcow2
-sudo rm -rf /var/lib/libvirt/images/vm-test-05-centos-7.1-x86_64.qcow2
 
 echo "Delete VMs from etc/hosts"
 echo "-------------------------"
@@ -22,6 +18,4 @@ echo "Remove ssh fingerprints"
 echo "-----------------------"
 ssh-keygen -R "vm-test-02"
 ssh-keygen -R "vm-test-03"
-ssh-keygen -R "vm-test-04"
-ssh-keygen -R "vm-test-05"
 
