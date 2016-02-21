a0abcf7120d5250533efbdfafbf3f2584e2feaa0
Yaacov Zamir <yzamir@gmail.com>
2016-02-21 08:38:20 +0200

    add labels to vms in hosts file

--- add-metric.sh
+++ add-metric.sh
@@ -8,12 +8,6 @@
 # oc delete sa --selector=metrics-infra -n openshift-infra
 # oc delete templates --selector=metrics-infra -n openshift-infra
 
-# add line to /etc/origin/master/master-config.yaml
-# assetConfig:
-# etricsPublicURL: https://vm-test-02/hawkular/metrics
-# and then reboot
-sed -i "/assetConfig:/a\ \ metricsPublicURL: https://vm-test-02/hawkular/metrics" /etc/origin/master/master-config.yaml
-
 # getting the openshift provider token
 # ------------------------------------
 
@@ -31,12 +25,6 @@ oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:opensh
 oc secrets new metrics-deployer nothing=/dev/null -n openshift-infra
 oc process -f metrics.yaml -v HAWKULAR_METRICS_HOSTNAME=vm-test-02,USE_PERSISTENT_STORAGE=false | oc create -n openshift-infra -f -
 
-# create route to hawkular
-# ------------------------
-
-# create route for comunicating with manageiq
-oadm router management-metrics --credentials=/etc/origin/master/openshift-router.kubeconfig --service-account=router --ports='443:5000' --selector='kubernetes.io/hostname=vm-test-02' --stats-port=1937 --host-network=false
-
 # check all
 # ---------
 
@@ -45,4 +33,16 @@ oc get pods -n openshift-infra
 
 # wait for casndra, hawkular and heapster pods to run then test
 curl -X GET https://vm-test-02/hawkular/metrics/status --insecure
+
+# create route to hawkular
+# ------------------------
+
+# add line to /etc/origin/master/master-config.yaml
+# assetConfig:
+# etricsPublicURL: https://vm-test-02/hawkular/metrics
+# and then reboot
+sed -i "/assetConfig:/a\ \ metricsPublicURL: https://vm-test-02/hawkular/metrics" /etc/origin/master/master-config.yaml
+
+# create route for comunicating with manageiq
+oadm router management-metrics --credentials=/etc/origin/master/openshift-router.kubeconfig --service-account=router --ports='443:5000' --selector='kubernetes.io/hostname=vm-test-02' --stats-port=1937 --host-network=false
 
