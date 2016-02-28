499025840e4346d802dc4e5000e1d6ab3d787d78
Yaacov Zamir <yzamir@gmail.com>
2016-02-28 18:18:47 +0200

    add a hint on getting the openshift token

--- add-metric.sh
+++ add-metric.sh
@@ -29,6 +29,11 @@ oadm router management-metrics --credentials=/etc/origin/master/openshift-router
 
 reboot
 
+# get the openshift token
+# -----------------------
+
+# secret=`oc get -n management-infra sa/management-admin --template='{{range .secrets}}{{printf "%s\n" .name}}{{end}}' | grep management-admin-token | head -n 1`; oc get -n management-infra secrets $secret --template='{{.data.token}}' | base64 -d > token.txt; cat token.txt; echo
+
 # cleaning up
 # -----------
 
