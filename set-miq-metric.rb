2f0bbf57886423525391f55e3a1ed8f19ec2b180
Yaacov Zamir <yzamir@gmail.com>
2016-03-21 15:48:34 +0200

    add random metrics for heatmap

--- set-miq-metric.rb
+++ set-miq-metric.rb
@@ -24,12 +24,27 @@ def push_metric (prov, timestamp, interval = "daily")
     :stat_containergroup_delete_rate  => rand(50))
 end
 
+def push_nodes_metric (prov, timestamp)
+  prov.container_nodes.each do |node|
+    node.metric_rollups << MetricRollup.create(
+      :capture_interval_name      => "hourly", 
+      :timestamp                  => timestamp,
+      :cpu_usage_rate_average     => rand(100), 
+      :mem_usage_absolute_average => rand(100), 
+      :derived_vm_numvcpus        => 4, 
+      :net_usage_rate_average     => rand(1000), 
+      :derived_memory_available   => 8192, 
+      :derived_memory_used        => rand(8192))
+  end
+end
+
 def push_last_30_days(prov)
-  (0 .. 30).each { |x|  push_metric(prov, x.days.ago, "daily") }
+  (0 .. 30).each { |x| push_metric(prov, x.days.ago, "daily") }
 end
 
 def push_last_24_hours(prov)
-  (0 .. 24).each { |x|  push_metric(prov, x.hours.ago, "hourly") }
+  (0 .. 24).each { |x| push_metric(prov, x.hours.ago, "hourly") }
+  (0 .. 24).each { |x| push_nodes_metric(prov, x.hours.ago) }
 end
 
 # --------------------
