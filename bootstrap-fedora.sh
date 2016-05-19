172a3d46004b1fe41ab27749b2e9cb39c9bf059b
Yaacov Zamir <yzamir@gmail.com>
2016-05-19 10:25:31 +0300

    bootstrap a minimal fedora install to run ansible openshift scripts

---
+++ bootstrap-fedora.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# run this file as root on master machine
+# e.g. (replace HOSTNAME-PREFIX with your chosen prefix):
+#   ssh root@vm-HOSTNAME-PREFIX-02.example.com 'bash -s HOSTNAME-PREFIX' < add-metric.sh
+# (or copy/checkout the script there and just run 'bash add-metric.sh HOSTNAME-PREFIX')
+
+if [ "$#" -eq 1 ]
+then
+  hostname="$1"
+else
+  echo "Usage: $0 <hostname>"
+  exit
+fi
+
+ssh-keygen -R $hostname
+ssh-copy-id root@$hostname
+
+scp /etc/yum.repos.d/fedora.repo "root@$hostname:/etc/yum.repos.d/fedora.repo"
+echo 'dnf upgrade -y' | ssh "root@$hostname"
+echo 'dnf groupinstall -y "System tools"' | ssh "root@$hostname"
+echo 'dnf install -y python2 python2-dnf libselinux-python libsemanage-python python2-firewall tar unzip bzip2' | ssh "root@$hostname"
+
