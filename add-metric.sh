78bb03c932a2294ebb392c847680ce705f5f25e1
Yaacov Zamir <yzamir@gmail.com>
2016-06-19 15:31:56 +0300

    update the add-metrics script

--- add-metric.sh
+++ add-metric.sh
@@ -15,32 +15,52 @@ fi
 # create the metric pods
 # ----------------------
 
-# creat the metric pods, and set authentications
-git clone https://github.com/openshift/origin-metrics.git
-cd origin-metrics/
-oc create -f metrics-deployer-setup.yaml -n openshift-infra
-oadm policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer -n openshift-infra
-oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster -n openshift-infra
-oc secrets new metrics-deployer nothing=/dev/null -n openshift-infra
-oc process -f metrics.yaml -v HAWKULAR_METRICS_HOSTNAME=$hostname,USE_PERSISTENT_STORAGE=false | oc create -n openshift-infra -f -
+# Switch to the openshift-infra project
+oc project openshift-infra
 
-# reboot
-# ------
+# Create a metrics-deployer service account
+oc create -f - <<API
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metrics-deployer
+secrets:
+- name: metrics-deployer
+API
 
-#reboot
+# Grante the edit permission for the openshift-infra project
+oadm policy add-role-to-user \
+    edit system:serviceaccount:openshift-infra:metrics-deployer
+
+# Grante the cluster-reader permission for the Heapster service account
+oadm policy add-cluster-role-to-user \
+    cluster-reader system:serviceaccount:openshift-infra:heapster
+
+# Using Generated Self-Signed Certificates
+oc secrets new metrics-deployer nothing=/dev/null
+
+# Deploying metrics without Persistent Storage
+wget https://raw.githubusercontent.com/openshift/origin-metrics/master/metrics.yaml
+oc new-app -f metrics.yaml -p USE_PERSISTENT_STORAGE=false -p HAWKULAR_METRICS_HOSTNAME=$hostname
+
+# Exit
+exit
 
 # get the openshift token
 # -----------------------
 
-# secret=`oc get -n management-infra sa/management-admin --template='{{range .secrets}}{{printf "%s\n" .name}}{{end}}' | grep management-admin-token | head -n 1`; oc get -n management-infra secrets $secret --template='{{.data.token}}' | base64 -d > token.txt; cat token.txt; echo
+secret=`oc get -n management-infra sa/management-admin --template='{{range .secrets}}{{printf "%s\n" .name}}{{end}}' | grep management-admin-token | head -n 1`; oc get -n management-infra secrets $secret --template='{{.data.token}}' | base64 -d > token.txt; cat token.txt; echo
 
 # cleaning up
 # -----------
 
-# oc delete all --selector=metrics-infra -n openshift-infra
-# oc delete secrets --selector=metrics-infra -n openshift-infra
-# oc delete sa --selector=metrics-infra -n openshift-infra
-# oc delete templates --selector=metrics-infra -n openshift-infra
+oc delete all --selector="metrics-infra"
+oc delete sa --selector="metrics-infra"
+oc delete templates --selector="metrics-infra"
+oc delete secrets --selector="metrics-infra"
+oc delete pvc --selector="metrics-infra"
+oc delete sa metrics-deployer
+oc delete secret metrics-deployer
 
 # check all
 # ---------
