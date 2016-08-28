311bf9eae0770485d4d4882d611519e8976a6c56
Yaacov Zamir <yzamir@gmail.com>
2016-08-28 15:44:56 +0300

    add write access to hawkular

--- add-metric.sh
+++ add-metric.sh
@@ -41,15 +41,14 @@ oc secrets new metrics-deployer nothing=/dev/null
 
 # Deploying metrics without Persistent Storage
 wget https://raw.githubusercontent.com/openshift/origin-metrics/master/metrics.yaml
-oc new-app -f metrics.yaml -p USE_PERSISTENT_STORAGE=false -p HAWKULAR_METRICS_HOSTNAME=$hostname
+oc new-app -f metrics.yaml -p USE_PERSISTENT_STORAGE=false -p USER_WRITE_ACCESS=true -p HAWKULAR_METRICS_HOSTNAME=$hostname 
 
 # Exit
 exit
 
 # get the openshift token
 # -----------------------
-
-secret=`oc get -n management-infra sa/management-admin --template='{{range .secrets}}{{printf "%s\n" .name}}{{end}}' | grep management-admin-token | head -n 1`; oc get -n management-infra secrets $secret --template='{{.data.token}}' | base64 -d > token.txt; cat token.txt; echo
+oc sa get-token management-admin -n management-infra > token.txt; cat token.txt; echo
 
 # cleaning up
 # -----------
