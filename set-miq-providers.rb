1928315261dce99d0510dade1d0f6bf5b3fdce51
yaacov <kobi.zamir@gmail.com>
2017-08-25 17:59:04 +0300

    update provider scripts

--- set-miq-providers.rb
+++ set-miq-providers.rb
@@ -50,7 +50,7 @@ end
 # Tokens
 # ------
 token1 =
-'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJtYW5hZ2VtZW50LWluZnJhIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im1hbmFnZW1lbnQtYWRtaW4tdG9rZW4taHY5M3EiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibWFuYWdlbWVudC1hZG1pbiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjFjY2FiOTZlLTZmODQtMTFlNy05ZDNjLTAwMWE0YTIzMTRkNSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDptYW5hZ2VtZW50LWluZnJhOm1hbmFnZW1lbnQtYWRtaW4ifQ.I2xG6ORylR7zxjJHb-DnNqKG5Ae37tYSYSvcsX4YpaW1ktY3oVT2CM9GBtDwkS6TY3o7wqvF4lpkYgGcv5rANLAgQT3WKFO1207RE1plTca85nMMIiZZkmsWpCa4U__PEOHv54LJ-hSHO8LTq1TlWRX0lL60Bxr-j79BmiEA05FN8u84uFRYJQ7JNWk-qII23eXOs4Zkq09zWUn8NGpeVa-mIszWvqUdrknvRtvvtCT0EkvKU5Odnugvi8DTaH_reInHdRo8y4_Csxem8SFAnCvEBzU4EIg1nUPSoD4sFGeljOC7v6EWg5q0408xmFBg1_oS5J8SeqNzZK57sCqn3w'
+'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJtYW5hZ2VtZW50LWluZnJhIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im1hbmFnZW1lbnQtYWRtaW4tdG9rZW4tbjZxMmsiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibWFuYWdlbWVudC1hZG1pbiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImE4ZjRhMzA5LTg3ZjMtMTFlNy05MDc4LTAwMWE0YTIzMTRkNSIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDptYW5hZ2VtZW50LWluZnJhOm1hbmFnZW1lbnQtYWRtaW4ifQ.FTWdK2S-XGj_ZmyvQNS7GhrkC3yvhpC1NEBY56q8szoTsmjpERGM7Bf30dTi3uJuPHa9w2bN20EL3fLbDdW2VmadS5heiC-a50RfcG_TyAX4yayZIO4bh3X7Cp8tOnJGVy95TmtbnOz9eEae477J7DeOWryMBhhmdwY0wDwt4DgfAsDXUohTweDaM7jBBZJDZNntw1OQKM_02U8ALeYmVbWtA-mRcWv5FOIvk0jVobSPr_rNr0CItx986nxIOQuhZIgtF2jBsn_NhduChIhArAuqBiHNDIhsEvNKioxATS69NQxny_LjEPAvLlZs_fBG9qljlu7rc8i6Wfqy7g90_w'
 token2 =
 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJtYW5hZ2VtZW50LWluZnJhIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6Im1hbmFnZW1lbnQtYWRtaW4tdG9rZW4tejJjZDciLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoibWFuYWdlbWVudC1hZG1pbiIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6ImExOGY3MTgwLTZkMTgtMTFlNy04ZGVkLTAwMWE0YTIzMTRkNiIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDptYW5hZ2VtZW50LWluZnJhOm1hbmFnZW1lbnQtYWRtaW4ifQ.cck_tbnkibOyYfZ4TeDSKNQTb-K3TZHzFNIsaSF5fxvxBQkPUC3iKjzkRYMFbuw6_kQ6aICItRNgRG6KfYwIlyE679W5g7QKsp8aEpgqbE9aWn6ipsBagnZpWK3U0Vglunl3dxngrA8z4aNRP4GvEbhVMSyedzzVPV1SYzoC071skokt6HwkDRnuL9lLZh9InWiHt85kE6gykPxXmbBvf9Cw1IWA0KeRupi6BWfdw8Em-ESNeE6Brhaiz7D8AyTACjJ0BC0XKB3-YCIOrb-XPqDWdl7BD9gMX1CFyW4N3eHDzBGHnf7sE2KAAI-97KDE37-TagUfqDnotoi2MLuFDw'
 token3 =
@@ -59,11 +59,11 @@ token3 =
 # ---------------------------
 # Delete all curent providers
 # ---------------------------
-#delete_providers
+delete_providers
 
 # ---------------------------
 # Create new curent providers
 # ---------------------------
-create_prov_with_auth('EngLab', 'yzamir-centos7-1.eng.lab.tlv.redhat.com', token1)
-create_prov_with_auth('EngLab-mohawk', 'yzamir-centos7-2.eng.lab.tlv.redhat.com', token2)
-create_prov_with_auth('EngLab-prometheus', 'yzamir-centos7-3.eng.lab.tlv.redhat.com', token3, 'prometheus.10.35.19.248.nip.io', :prometheus)
+create_prov_with_auth('EngLab', 'yzamir-centos7-1.eng.lab.tlv.redhat.com', token1, 'prometheus.10.35.19.246.nip.io', :prometheus)
+#create_prov_with_auth('EngLab-mohawk', 'yzamir-centos7-2.eng.lab.tlv.redhat.com', token2)
+#create_prov_with_auth('EngLab-prometheus', 'yzamir-centos7-3.eng.lab.tlv.redhat.com', token3, 'prometheus.10.35.19.248.nip.io', :prometheus)
